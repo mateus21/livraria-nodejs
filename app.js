6023618772f44467f655c74f0290498d1fe0c69e
Mateus Mantovani <mateus@codebit.com.br>
2018-09-07 16:24:10 -0300

    melhorando um pouco com express e EJS

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/produtos', function(req, res) {
-    res.send("<html><body><h1>Listagem de produtos</h1></body></html>");
-});
+app.set('view engine', 'ejs');
+
 
 app.get('/', function(req, res) {
     res.send("<html><body><h1>Olá Mundo!</h1></body></html>");
 });
+
+
+app.get('/produtos', function(req, res) {
+    res.render("produtos/lista");
+});
+
+
 
 
 app.listen(3000, function(){
