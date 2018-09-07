cb6971e99522d37119c6f96012547bfee54584bd
Mateus Mantovani <mateus@codebit.com.br>
2018-09-07 15:57:24 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/produtos', function(req, res) {
+    res.send("<html><body><h1>Listagem de produtos</h1></body></html>");
+});
+
+app.get('/', function(req, res) {
+    res.send("<html><body><h1>Olá Mundo!</h1></body></html>");
+});
+
+
+app.listen(3000, function(){
+    console.log("Servidor rodando na porta 3000");
+});
