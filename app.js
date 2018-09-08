77a84260c06444e7b8c792c7b195e568579f9036
Mateus Mantovani <mateus@codebit.com.br>
2018-09-08 12:10:34 -0300

    Melhorando a estrutura base do projeto

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
+var app = require('./config/express')();
+var productsRoute = require('./app/routes/products')(app);
 
 
 app.get('/', function(req, res) {
     res.send("<html><body><h1>Olá Mundo!</h1></body></html>");
 });
-
-
-app.get('/produtos', function(req, res) {
-    res.render("produtos/lista");
-});
-
-
 
 
 app.listen(3000, function(){
