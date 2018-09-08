6dd30dc8f9987c30a8d09d8e23cc70e19222cec9
Mateus Mantovani <mateus@codebit.com.br>
2018-09-08 18:41:59 -0300

    finalizando mecânica de cadastro de livros

--- app/infra/ProductsDAO.js
+++ app/infra/ProductsDAO.js
@@ -6,6 +6,10 @@ ProductsDAO.prototype.list = function(callback) {
     this._connection.query('select * from livros', callback);
 }
 
+ProductsDAO.prototype.save = function(product, callback) {
+    this._connection.query('insert into livros SET ?', product, callback);
+}
+
 module.exports = function() {
     return ProductsDAO;
 }
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,13 +1,29 @@
 module.exports = function (app) {
     app.get('/produtos', function(req, res) {
         var connection = app.infra.connectionFactory();
-        var productsDB = new app.infra.ProductsDAO(connection);
+        var productsDAO = new app.infra.ProductsDAO(connection);
 
-        productsDB.list(function(err, results) {
+        productsDAO.list(function(err, results) {
             res.render('produtos/lista', {lista:results});
         });
 
         connection.end();
+
+    });
+
+    app.get('/produtos/form', function(req, res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos', function(req, res) {
+        var product = req.body;
+        
+        var connection = app.infra.connectionFactory();
+        var productsDAO = new app.infra.ProductsDAO(connection);
+
+        productsDAO.save(product, function(err, results) {
+            res.redirect('/produtos');
+        });
 
     });
 }
